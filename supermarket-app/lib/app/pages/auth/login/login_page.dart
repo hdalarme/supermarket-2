6dec35c8f5807481a5ea8b5f7ae9be03d0f974b9
Helbertt Dalarme <hdalarme@yahoo.com.br>
2023-03-14 11:04:13 -0300

    Login

--- supermarket-app/lib/app/pages/auth/login/login_page.dart
+++ supermarket-app/lib/app/pages/auth/login/login_page.dart
@@ -1,80 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supermarket/app/core/ui/style/text_styles.dart';
 import 'package:supermarket/app/core/ui/widgets/appbar.dart';
 import 'package:supermarket/app/core/ui/widgets/button.dart';
+import 'package:supermarket/app/pages/auth/login/login_controller.dart';
+import 'package:supermarket/app/pages/auth/login/login_state.dart';
+import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatelessWidget {
+import '../../../core/ui/base_state/base_state.dart';
 
-  const LoginPage({ super.key });
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: Appbar(),
-           body: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Form(
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends BaseState<LoginPage, LoginController> {
+  final formKey = GlobalKey<FormState>();
+
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginController, LoginState>(
+      listener: (context, state) {
+        state.status.matchAny(
+            any: () => hideLoader(),
+            login: () => showLoader(),
+            error: () {
+              hideLoader();
+              showError('Login ou senha Inválidos!');
+            },
+            loginError: () {
+              hideLoader();
+              showError('Erro ao registrar usuário');
+            },
+            success: () {
+              hideLoader();
+              Navigator.pop(context, true);
+            });
+      },
+      child: Scaffold(
+        appBar: Appbar(),
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                    key: formKey,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Login', style: context.textStyles.textTitle,),
+                        Text(
+                          'Login',
+                          style: context.textStyles.textTitle,
+                        ),
                         const SizedBox(
-                           height: 30,
+                          height: 30,
                         ),
                         TextFormField(
-                          decoration: const InputDecoration(labelText: 'E-mail'),
+                          decoration:
+                              const InputDecoration(labelText: 'E-mail'),
+                          controller: emailEC,
+                          validator: Validatorless.multiple([
+                            Validatorless.required('E-mail obrigatório'),
+                            Validatorless.email('E-mail inválido'),
+                          ]),
                         ),
                         const SizedBox(
-                           height: 30,
+                          height: 30,
                         ),
                         TextFormField(
                           decoration: const InputDecoration(labelText: 'Senha'),
+                          controller: passwordEC,
+                          validator: Validatorless.multiple([
+                            Validatorless.required('Senha obrigatória'),
+                          ]),
                         ),
                         const SizedBox(
-                           height: 50,
+                          height: 50,
                         ),
-                        Center(child: Button(
-                          label: 'Entar',
-                          width: double.infinity, 
-                          onPressed: () {  },
-                        ),),
+                        Center(
+                          child: Button(
+                            label: 'Entar',
+                            width: double.infinity,
+                            onPressed: () {
+                              final valid =
+                                  formKey.currentState?.validate() ?? false;
+
+                              if (valid) {
+                                controller.Login(emailEC.text, passwordEC.text);
+                              }
+                            },
+                          ),
+                        ),
                       ],
-                    )
+                    )),
+              ),
+            ),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Não possui uma conta',
+                          style: context.textStyles.textBold),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/auth/register');
+                          },
+                          child: Text(
+                            'Cadastre-se',
+                            style: context.textStyles.textBold
+                                .copyWith(color: Colors.blue),
+                          ))
+                    ],
                   ),
                 ),
               ),
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Não possui uma conta', 
-                          style: context.textStyles.textBold
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed('/auth/register');
-                          }, 
-                          child: Text(
-                            'Cadastre-se',
-                            style: context.textStyles.textBold.copyWith(color: Colors.blue),
-                          )
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            ],
-           ),
-       );
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- supermarket-app/lib/app/supermarket_app.dart
+++ supermarket-app/lib/app/supermarket_app.dart
@@ -2,30 +2,29 @@ import 'package:flutter/material.dart';
 
 import 'package:supermarket/app/core/provider/application_binding.dart';
 import 'package:supermarket/app/core/ui/theme/theme_config.dart';
-import 'package:supermarket/app/pages/auth/login/login_page.dart';
+import 'package:supermarket/app/pages/auth/login/login_router.dart';
 import 'package:supermarket/app/pages/auth/register/register_router.dart';
 import 'package:supermarket/app/pages/purchase_list/purchase_list_router.dart';
 import 'package:supermarket/app/pages/purchase_list_item/purchase_list_item_router.dart';
 import 'package:supermarket/app/pages/splash/splash_page.dart';
 
 class SupermarketApp extends StatelessWidget {
+  const SupermarketApp({super.key});
 
-  const SupermarketApp({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return ApplicationBinding(
-         child: MaterialApp(
-          title: 'Supermarket',
-          theme: ThemeConfig.theme,
-          routes: {
-            '/': (context) => const SplashPage(),
-            '/home': (context) => PurchaseListRouter.page,
-            '/listItem': (context) => PurchaseListItemRouter.page,
-            '/auth/login':(context) => const LoginPage(),
-            '/auth/register':(context) => RegisterRouter.page,
-          },
-         ),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return ApplicationBinding(
+      child: MaterialApp(
+        title: 'Supermarket',
+        theme: ThemeConfig.theme,
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/home': (context) => PurchaseListRouter.page,
+          '/listItem': (context) => PurchaseListItemRouter.page,
+          '/auth/login': (context) => LoginRouter.page,
+          '/auth/register': (context) => RegisterRouter.page,
+        },
+      ),
+    );
   }
 }
